bbf3686bb0e3cd66b3a5b0e4c1fc1f0d76f39e57
Sinzeta <sinzeta@proton.me>
2026-09-05 21:04:00 +0200

    Add SQLAlchemy session factory

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 class Base(DeclarativeBase):
     pass
