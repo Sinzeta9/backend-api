cf45d583859793c9b79b5ce549784c1adb169a23
Sinzeta <sinzeta@proton.me>
2026-09-05 17:26:55 +0200

    Separate application configurations

--- app/database.py
+++ app/database.py
@@ -1,11 +1,6 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+from app.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
