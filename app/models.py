9a863dfe1153f3ff1eb9fb6eebf0786c2ee03556
Sinzeta <sinzeta@proton.me>
2026-09-06 19:07:22 +0200

    Add descripcion migration to prueba

--- app/models.py
+++ app/models.py
@@ -9,4 +9,4 @@ class Prueba(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     nombre: Mapped[str] = mapped_column(String(100))
-
+    descripcion: Mapped[str | None] = mapped_column(String(255), nullable=True)
